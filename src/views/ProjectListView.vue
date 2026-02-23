c2f387063a1d218694bc01404dde5a8d707e7527
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-02-23 14:50:09 +0100

    update projects

--- src/views/ProjectListView.vue
+++ src/views/ProjectListView.vue
@@ -4,6 +4,12 @@
     <a href="https://github.com/bruteforce774/">Besøk gjerne min Github</a>. Der har jeg blant annet:
   </p>
   <p>
+    En <a href="https://bruteforce774.github.io/conway/">simulering av Conways Game of Life</a> i vanilla JavaScript.
+  </p>
+  <p>
+    En <a href="https://github.com/bruteforce774/vue-sandbox/">liten nettbutikkapp i Vue</a> som jeg leverte til en frontendeksamen.
+  </p>
+  <p>
     En <a href="https://github.com/bruteforce774/FullstackBookshelf">bokhylleapp</a> med en backend i C#.
   </p>
   <p>
@@ -11,8 +17,5 @@
   </p>
   <p>
     En <a href="https://github.com/bruteforce774/speak">væroppleser</a> skrevet i Python.
-  </p>
-  <p>
-    Sjekk også min <a href="https://bruteforce774.github.io/conway/">simulering av Conways Game of Life</a> i vanilla JavaScript.
   </p>
 </template>
